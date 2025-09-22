024f21c5d089d26a57a112da351f59b53d0b6fea
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-09-22 12:29:14 +0100

    minor

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,7 +4,7 @@
         <v-sheet class="hero-section">
       
       <v-container fluid class="pa-0 text-center fill-height pb-0">
-        <v-row ustify="center" align="end" class="fill-height fill-width">
+        <v-row ustify="center" align="end" class="fill-height">
           <v-col cols="12" sm="10" md="8" lg="6" xl="5">
             <div class="hero-content">
               <h1 class="hero-title text-left text-blue-darken-3 font-weight-bold">
